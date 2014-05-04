c272e1176eb6b90b16c56b52c1e29163a2bcdf0d
Julian Bogdani <jbogdani@gmail.com>
2014-05-04 18:10:34 +0200

    3.2.0 Code cleaning

--- index.php
+++ index.php
@@ -40,81 +40,77 @@ try
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <title>BraDypUS <?php echo version::current() . ($_SESSION['app'] ?  ' :: ' . strtoupper(cfg::main('name')) : '' ); ?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<?php
-	/*
-	 * Styles loader
-	 */
-	utils::css( array (
-		'main.css'
-	), $_GET);
-	
-	?>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <?php
+  /*
+   * Styles loader
+   */
+  utils::css( array (
+    'main.css'
+  ), $_GET);
 
-	
-	
+  ?>
+
+  <script src="./controller.php?obj=tr&method=lang2json&param=true"></script>
+  <script> var debugMode = <?php echo  ($_GET['debug'] ? 'true' : 'false');?></script>
+  <?php
+  utils::js( array(
+    'php2js.js',
+    'jquery-2.1.0.min.js',
+    'jquery-sortable.js',
+    'bootstrap.js',
+    'bootstrap-datepicker.js',
+    'jquery.dataTables.js',
+    'datatables-bootstrap.js',
+    'jquery.keyboard.js',
+    'utils.js',
+    'core.js',
+    'jquery.pnotify.js',
+    'api.js',
+    'layout.js',
+    'jquery.fineuploader-3.4.0.js',
+    'formControls.js',
+    'select2.js',
+    'enhanceForm.js',
+    'jquery.checklabel.js',
+    'jquery.printElement.js',
+    'jquery.jqplot.js',
+    'jqplot.barRenderer.min.js',
+    'jqplot.categoryAxisRenderer.min.js',
+    'jqplot.pointLabels.js',
+    'export-jqplot-to-png.js',
+    'jquery.insertAtCaret.js',
+    'bootstrap-slider.js'
+
+  ), $_GET );
+  ?>
+  <script>
+    $(document).ready(function(){
+      layout.init();
+<?php if ( utils::canUser('enter') ): ?>
+      layout.loadHome();
+      layout.hashActions();
+<?php elseif ($_REQUEST['address'] && $_REQUEST['token']): ?>
+      core.runMod('login', false, function(){
+        login.loadLogin();
+        login.loadResetPwd('<?php echo $_REQUEST['app'] . "', '" . $_REQUEST['address'] . "', '" . $_REQUEST['token'];?>');
+      });
+<?php else : ?>
+      core.runMod('login', false, function(){
+        login.autologin(<?php echo $_REQUEST['app'] ? "'" . $_REQUEST['app'] . "'" : ''; ?>);
+        login.loadLogin('<?php echo $_REQUEST['app']; ?>');
+      });
+<?php endif; ?>
+    });
+<?php if(utils::is_online()) : ?>
+		var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-10461068-18']);_gaq.push(['_trackPageview']);(function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();
+<?php endif;?>  
+  </script>
 </head>
 
-	<body<?php echo !utils::canUser('enter') ? ' class="login"' : '' ?>>
-	
-	<script src="./controller.php?obj=tr&method=lang2json&param=true"></script>
-	<script> var debugMode = <?php echo  ($_GET['debug'] ? 'true' : 'false');?></script>
-	<?php
-	utils::js( array(
-		'php2js.js',
-		'jquery-2.1.0.min.js',
-		'jquery-sortable.js',
-		'bootstrap.js',
-		'bootstrap-datepicker.js',
-		'jquery.dataTables.js',
-		'datatables-bootstrap.js',
-		'jquery.keyboard.js',
-		'utils.js',
-		'core.js',
-		'jquery.pnotify.js',
-		'api.js',
-		'layout.js',
-		'jquery.fineuploader-3.4.0.js',
-		'formControls.js',
-		'select2.js',
-		'enhanceForm.js',
-		'jquery.checklabel.js',
-		'jquery.printElement.js',
-		'jquery.jqplot.js',
-		'jqplot.barRenderer.min.js',
-		'jqplot.categoryAxisRenderer.min.js',
-		'jqplot.pointLabels.js',
-		'export-jqplot-to-png.js',
-		'jquery.insertAtCaret.js',
-		'bootstrap-slider.js'
-	
-	), $_GET );
-	?>
-	<script>
-	$(document).ready(function(){
-		layout.init();
-<?php if ( utils::canUser('enter') ): ?>
-		layout.loadHome();
-		layout.hashActions();
-<?php elseif ($_REQUEST['address'] && $_REQUEST['token']): ?>
-		core.runMod('login', false, function(){
-			login.loadLogin();
-			login.loadResetPwd('<?php echo $_REQUEST['app'] . "', '" . $_REQUEST['address'] . "', '" . $_REQUEST['token'];?>');
-		});
-<?php else : ?>
-		core.runMod('login', false, function(){
-			login.autologin(<?php echo $_REQUEST['app'] ? "'" . $_REQUEST['app'] . "'" : ''; ?>);
-			login.loadLogin('<?php echo $_REQUEST['app']; ?>');
-		});
-<?php endif; ?>
-	});
-	<?php if(utils::is_online()) : ?>
-		var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-10461068-18']);_gaq.push(['_trackPageview']);(function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();
-	<?php endif;?>
-	</script>
-	</body>
+<body<?php echo !utils::canUser('enter') ? ' class="login"' : '' ?>></body>
 	
 </html>
 
