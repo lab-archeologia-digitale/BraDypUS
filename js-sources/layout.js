0878875c4c88f20d661a349e83b85df03b7b3c5a
Julian Bogdani <jbogdani@gmail.com>
2016-09-02 22:06:26 +0200

    Bug fixed with main table menu

--- js-sources/layout.js
+++ js-sources/layout.js
@@ -48,7 +48,7 @@ var layout = {
 			}, 'html');
 
 			$(document).delegate('select.tb', 'change', function(e){
-				$(this).parent('li.tb').next('li.tb_opt').hide().load('./controller.php?module=home/table_menu&tb=' + $(this).val()).fadeIn('slow');
+				$(this).parents('li.tb').next('li.tb_opt').hide().load('./controller.php?module=home/table_menu&tb=' + $(this).val()).fadeIn('slow');
 			});
 
 			$(document).delegate('input.fast_search', 'keyup', function(e){
