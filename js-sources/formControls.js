f07ed5f0ee2c0302e8730e568f190af033cedad2
Julian Bogdani <jbogdani@gmail.com>
2016-09-03 16:51:13 +0200

    Minor bug with regex check: empty values are not tested

--- js-sources/formControls.js
+++ js-sources/formControls.js
@@ -4,18 +4,18 @@
  * @license			See file LICENSE distributed with this code
  * @since			10/mag/2011
  * @uses			core.message
- * 
- * 
+ *
+ *
  */
 
 function formControl(el, options){
-	
+
 	// element
 	var $this = $(el);
-	
+
 	// array filled with errors
 	var wrongEl = [];
-	
+
 	//default settings
 	var settings = {
 			addChange	: true,
@@ -36,22 +36,22 @@ function formControl(el, options){
 				no_rules_for	: core.tr('no_rules_for')
 			}
 	};
-	
+
 	// extend default settings with options
 	if (options){
 		settings = $.extend(settings, options);
 	}
-	
+
 	// add changed attribute to form inputs on change event
 	if ( settings.addChange ) {
 		$this.find (':input').change(function(){
 			$(this).attr('changed', 'auto');
 		});
 	}
-	
+
 	// public method observe
 	this.observe = function(){
-		
+
 		// reset wrongEl array
 		wrongEl = [];
 
@@ -67,19 +67,19 @@ function formControl(el, options){
 				});
 			}
 		});
-		
+
 		return this;
 	};
-	
+
 	// public method: Checks the form for errors;
 	this.check = function(){
-		
+
 		// reset wrongEl array
 		wrongEl = [];
-		
-		// remove previous errors from form 
+
+		// remove previous errors from form
 		removeError();
-		
+
 		var checkTypes = [ 'not_empty', 'int', 'email', 'no_dupl', 'range', 'regex' ];
 
 		$.each(checkTypes, function(index, id){
@@ -90,69 +90,71 @@ function formControl(el, options){
 				});
 			}
 		});
-		
+
 		return this;
 	};
-	
+
 	/**
 	 * Sends form with controles:
 	 *	checks if the wrongEl contains errors; if yes form will not be send; an error message will appear
-	 *	only inputs with 'changed' tags will be sent!  
+	 *	only inputs with 'changed' tags will be sent!
 	 */
 	this.send = function (all) {
-		
+
 		// checks for pesent errors
 		if ( wrongEl.length > 0 ) {
-		
+
 			core.message(settings.msg.errors_in_form, 'error');
-			
+
 		} else {
-			
+
+			var ser;
+
 			if (all){
-				
-				var ser = $this.serialize();
-				
+
+				ser = $this.serialize();
+
 			} else {
-				
+
 				// disable unchanged inputs!
 				var not_changed = $this.find(':input:not([changed])');
-				
+
 				not_changed.attr('disabled','disabled');
-		
+
 				// serialize changed inputs
-				var ser = $this.serialize();
-		
+				ser = $this.serialize();
+
 				// re-enable inputs
 				not_changed.removeAttr('disabled');
 			}
-			
-			
+
+
 			if ( !ser ) {
-				
+
 				core.message(settings.msg.no_data_to_save, 'error');
-				
+
 			} else {
-				
+
 				$.post(
 						settings.submitURL,
 						ser,
 						function(data){
-							
+
 							if (data.status == 'success'){
-								
+
 								// remove changed tags if there is a successful response from server!
 								$this.find(':input[changed="auto"]').removeAttr('changed');
-	
+
 								core.message(data.verbose, 'success');
-								
+
 								settings.success(data);
-								
+
 							} else if (data.status == 'error') {
-								
+
 								core.message(data.verbose, 'error');
-								
+
 							} else {
-								
+
 								core.message(data.verbose, 'error');
 							}
 						},
@@ -160,10 +162,10 @@ function formControl(el, options){
 				);
 			}
 		}
-		
+
 		return this;
 	};
-	
+
 	this.option = function(key, value){
 		if(value){
 			settings[key] = value;
@@ -172,7 +174,7 @@ function formControl(el, options){
 			return settings[key];
 		}
 	};
-	
+
 	// private method
 	var styleError = function(input, checkType){
 		if ( !input.hasClass('notValid') ) {
@@ -189,35 +191,35 @@ function formControl(el, options){
 		}
 		input.addClass('notValid');
 	};
-	
-	
+
+
 	// private method: Removes error style (class) and text from form or from element
 	var removeError = function(el){
-		
+
 		if ( el ) {
 			$(el).next('span.notValid').remove();
 			$(el).removeClass('notValid');
-			
+
 		} else {
-			
+
 			$this.find('span.notValid').remove();
 			$this.find(':input.notValid').removeClass('notValid');
-			
+
 		}
 	};
-	
-	
-	
+
+
+
 	/**
 	 *	Main check function:
 	 *		checks input value using checkType
-	 *		if check is not successful input will be send to wrongEl array and input will be marked with error text and style 
+	 *		if check is not successful input will be send to wrongEl array and input will be marked with error text and style
 	 */
 	var checkInput = function(input, checkType){
-		
+
 
 		var val = input.val();
-		
+
 		switch ( checkType ) {
 
 			case 'int':
@@ -228,14 +230,14 @@ function formControl(el, options){
 				break;
 			case 'email':
 				var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-				
-				if ( val!= '' && !emailPattern.test(val) ) {
+
+				if ( val !== '' && !emailPattern.test(val) ) {
 					styleError(input, settings.msg[checkType]);
 					wrongEl.push(input);
 				}
 				break;
 			case 'not_empty':
-				if ( val == '' ) {
+				if ( val === '' ) {
 					styleError(input, settings.msg[checkType]);
 					wrongEl.push(input);
 				}
@@ -260,7 +262,7 @@ function formControl(el, options){
 			case 'range':
 				var min = parseInt(input.attr('min'));
 				var max = parseInt(input.attr('max'));
-				
+
 				val = parseInt(val);
 
 				if ( val < min || val > max  || isNaN(val)) {
@@ -268,23 +270,22 @@ function formControl(el, options){
 					wrongEl.push(input);
 				}
 				break;
-				
+
 			case 'regex':
 				var mypattern = input.attr('mypattern');
 				var pattern = new RegExp (mypattern);
-				if (!pattern.test(val)) {
+				if (val && !pattern.test(val)) {
 					styleError(input, settings.msg[checkType] + ' (' + mypattern+ ')');
 					wrongEl.push(input);
 				}
 				break;
-				
+
 			default:
 				console.log(settings.msg.no_rules_for + ' ' + checkType);
 				break;
 
 		}
 	};
-	
+
 	return this;
 }
-
