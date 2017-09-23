8a606ddda1ede4830610b8c2eb0c8252b81081b0
Julian Bogdani <jbogdani@gmail.com>
2017-09-23 21:03:49 +0200

    Code cleanup

--- modules/geoface2/geoface2.php
+++ modules/geoface2/geoface2.php
@@ -17,36 +17,31 @@ class geoface2_ctrl extends Controller
 		$tb = $this->request['tb'];
 		$id = $this->request['id'][0];
 		$geometry = $this->request['coords'];
-		
-		try
-		{
-			if (!utils::canUser('add_new'))
-			{
+
+		try {
+
+			if (!utils::canUser('add_new')) {
 				throw new myException('User has not enough privilege to add a new record');
 			}
-			
+
 			$record = new Record($tb, $id, new DB());
-			
+
 			$new_id = $record->addGeodata($geometry);
-			
-			if ($new_id)
-			{
+
+			if ($new_id) {
 				utils::response('ok_insert_geodata', false, false, array('id'=>$new_id));
-			}
-			else
-			{
+			} else {
 				throw new myException('Insert geodata query returned false');
 			}
-		}
-		catch (myException $e)
-		{
+
+		} catch (myException $e) {
 			$e->log();
 			utils::response('error_insert_geodata', 'error');
 		}
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Erases geoemtry from geodatada table
 	 * @throws myException
@@ -54,38 +49,33 @@ class geoface2_ctrl extends Controller
 	public function erase()
 	{
 		$id_arr = $this->post['ids'];
-		
-		try
-		{
-			if (!utils::canUser('edit'))
-			{
+
+		try {
+
+			if (!utils::canUser('edit')) {
 				throw new myException('User has not enough privilege to edit records');
 			}
-			
+
 			$record = new Record('novalue', false, new DB());
-			
-			foreach ($id_arr as $id){
-				if (!$record->deleteGeodata($id)){
+
+			foreach ($id_arr as $id) {
+				if (!$record->deleteGeodata($id)) {
 					$error = true;
 				}
 			}
-			
-			if (!$error)
-			{
+
+			if (!$error) {
 				utils::response('ok_delete_geodata');
-			}
-			else
-			{
+			} else {
 				throw new myException('Delete geodata query returned false');
 			}
-		}
-		catch (myException $e)
-		{
+
+		} catch (myException $e) {
 			$e->log();
 			utils::response('error_delete_geodata', 'error');
 		}
 	}
-	
+
 	/**
 	 * Updates geometri info in geodata table
 	 * @throws myException
@@ -93,43 +83,37 @@ class geoface2_ctrl extends Controller
 	public function update()
 	{
 		$post = $this->post['geodata'];
-		
-		try
-		{
-			if (!utils::canUser('edit'))
-			{
+
+		try {
+			if (!utils::canUser('edit')) {
 				throw new myException('User has not enough privilege to edit records');
 			}
-			
+
 			$db = new DB();
-			
+
 			foreach ($post as $row){
 				$ret = $db->query('UPDATE `' . PREFIX . '__geodata` SET ' .
 						  '`geometry` = :geometry ' .
-						  'WHERE `id` = ' . $row['id'], 
+						  'WHERE `id` = ' . $row['id'],
 						  array(':geometry'=>$row['coords']), 'boolean');
-				if (!$ret){
+				if (!$ret) {
 					$error = true;
 				}
 			}
-				
-			if (!$error)
-			{
+
+			if (!$error) {
 				utils::response('ok_update_geometry');
-			}
-			else
-			{
+			} else {
 				throw new myException('Update geometry query returned false');
 			}
-		}
-		catch (myException $e)
-		{
+
+		} catch (myException $e) {
 			$e->log();
 			utils::response('error_update_geometry', 'error');
 		}
 	}
-	
-	
+
+
 	/**
 	 * Returns complex object with metadata and geodata to use for mapping
 	 * @return string
@@ -138,92 +122,85 @@ class geoface2_ctrl extends Controller
 	{
 		$tb = $this->request['tb'];
 		$where = $this->request['where'];
-		try
-		{
+
+		try {
+
 			$geoface = cfg::tbEl($tb, 'geoface');
-			
-			if (!$geoface['epsg'])
-			{
+
+			if (!$geoface['epsg']) {
 				$geoface['epsg'] = '4326';
 			}
-			if (!is_array($geoface['layers']['web']))
-			{
+
+			if (!is_array($geoface['layers']['web'])) {
 				$geoface['layers']['web'] = array();
 			}
-	
-			if (is_array($geoface['layers']['local']))
-			{
-				foreach ($geoface['layers']['local'] as &$vec)
-				{
+
+			if (is_array($geoface['layers']['local'])) {
+				foreach ($geoface['layers']['local'] as &$vec) {
 					$vec['id'] = PROJ_GEO_DIR. $vec['id'];
 				}
 			}
-	
+
 			$preview = cfg::getPreviewFlds($tb);
-			
-			$part = array();
-	
-			foreach ($preview as $fldid)
-			{
-				if ($fldid != 'id')
-				{
-					$part[] = '`' . $tb . '`.`' . $fldid . '` AS `' . cfg::fldEl($tb, $fldid, 'label') . '`';
+
+			$part = [];
+
+			foreach ($preview as $fldid) {
+				if ($fldid != 'id') {
+					array_push($part, '`' . $tb . '`.`' . $fldid . '` AS `' . cfg::fldEl($tb, $fldid, 'label') . '`');
 				}
 			}
 
 			$where ? $where = base64_decode($where) : '';
-			
-			if (!preg_match('/`' . $tb . '`\.`id`/', $where) && $where)
-			{
+
+			if (!preg_match('/`' . $tb . '`\.`id`/', $where) && $where) {
 				$where = str_replace('`id`', '`' . $tb . '`.`id`', $where);
 			}
+
 			$sql = 'SELECT `' . $tb . '`.`id`  AS `id`, ' . implode(', ', $part) . ', `' . PREFIX . '__geodata`.`id` AS `geo_id`,  `geometry` '
 			. ' FROM `' . $tb . '` LEFT JOIN `' . PREFIX . '__geodata` '
 			. " ON `" . $tb . "`.`id` = `" . PREFIX . "__geodata`.`id_link` AND `" . PREFIX . "__geodata`.`table_link` = '" . $tb . "' "
 			. ' WHERE `geometry` IS NOT NULL '
 			. ($where ? ' AND ' . $where : '');
-				
+
 			$db = new DB();
-			
+
 			$res = $db->query($sql, false);
-			
-			if($res)
-			{
+
+			if($res) {
+
 				$geo = new sql2geoJSON();
-					
-				foreach($res as $row)
-				{
+
+				foreach($res as $row) {
 					$geo->addFeature($row);
 				}
+
 				$response['status'] = 'success';
 				$response['data'] = $geo->getObject();
-				
-			}
-			else if (!$res AND (trim($where) == '1' || !$where) && utils::canUser('add_new'))
-			{
+
+			} else if (!$res AND (trim($where) == '1' || !$where) && utils::canUser('add_new')) {
+
 				$response['status'] = 'warning';
 				$response['data'] = '';
-			}
-			else
-			{
+
+			} else {
 				utils::response('no_geodata_available', 'error');
 				return;
 			}
-			
-			$response['metadata'] = array_merge($geoface, array(
-					'tb_id'=>$tb,
-					'tb'=>cfg::tbEl($tb, 'label'),
-					'canUserEdit' => utils::canUser('edit')
-			));
-			
+
+			$response['metadata'] = array_merge($geoface, [
+				'tb_id'=>$tb,
+				'tb'=>cfg::tbEl($tb, 'label'),
+				'canUserEdit' => utils::canUser('edit')
+			]);
+
 			echo json_encode($response);
-		}
-		catch (myException $e)
-		{
+
+		} catch (myException $e) {
 			$e->log();
 			utils::response('error_getting_geodata', 'error');
 		}
 	}
-	
-	
+
+
 }
