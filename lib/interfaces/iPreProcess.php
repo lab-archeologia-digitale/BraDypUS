46b424bea57345cb8328a24eca01899846ab2e72
Julian Bogdani <jbogdani@gmail.com>
2018-07-26 11:40:16 +0200

    Added support for preprocess in API

---
+++ lib/interfaces/iPreProcess.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author			Julian Bogdani <jbogdani@gmail.com>
+ * @copyright		BraDypUS, Julian Bogdani <jbogdani@gmail.com>
+ * @license			See file LICENSE distributed with this code
+ * @since			May 2, 2018
+ */
+
+interface iPreProcess
+{
+	/**
+	 * Runs middlevare
+	 * @param  array $get  array of GET data
+	 * @param  array $post  array of POST data
+	 * @return array 				halt: boolean echo: string: headers: array
+	 */
+	public function run($get = false, $post = false);
+
+
+}
