354a0350b09e8d76ac1887845e46ea73a609a630
Julian Bogdani <jbogdani@gmail.com>
2020-08-02 14:43:14 +0200

    Fixed controller

--- modules/myHistory/myHistory.mini.js
+++ modules/myHistory/myHistory.mini.js
@@ -1,4 +1,4 @@
 /*
  * copyright BraDypUS 
  */
-var myHistory={init:function(){$.get('controller.php?obj=myHistory_ctrl&method=show_all',function(data){if(data&&data!=='<pre></pre>'){core.open({html:data,title:core.tr('history')});}else{core.message(core.tr('history_is_empty'));}});}};
+var myHistory={init:function(){$.get('./?obj=myHistory_ctrl&method=show_all',function(data){if(data&&data!=='<pre></pre>'){core.open({html:data,title:core.tr('history')});}else{core.message(core.tr('history_is_empty'));}});}};
