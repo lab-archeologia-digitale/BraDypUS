3d622cd94803f88c60b58805b939dfb952e687d4
Julian Bogdani <jbogdani@gmail.com>
2020-08-02 12:47:04 +0200

    Refactored constants

--- lib/constants.php
+++ lib/constants.php
@@ -5,61 +5,66 @@
  * @license			See file LICENSE distributed with this code
  * @since			Apr 7, 2012
  */
-/*
- * Main CONSTANTS
- */
-
-define ( 'MAIN_DIR',	$basePath);
-define ( 'LIB_DIR',		MAIN_DIR . 'lib/' );
-define ( 'MOD_DIR',		MAIN_DIR . 'modules/' );
-define ( 'LOCALE_DIR',	MAIN_DIR . 'locale/');
-$error_log = MAIN_DIR . 'logs/error.log';
 
 // required since 5.0.1: http://php.net/manual/en/function.date-default-timezone-set.php
 date_default_timezone_set('Europe/Rome');
 
-// SETS ERROR REPORTING
-( ini_get('error_reporting') <> 6135 )	?	ini_set('error_reporting', 6135) : '';
+/**
+ * Main CONSTANTS are always set
+ */
+define ( 'MAIN_DIR',	$basePath);
+define ( 'LIB_DIR',		MAIN_DIR . 'lib/' );
+define ( 'MOD_DIR',		MAIN_DIR . 'modules/' );
+define ( 'LOCALE_DIR',	MAIN_DIR . 'locale/');
+define ( 'PREFIX_DELIMITER', '__');
 
+// No project available: app error log
+$error_log = MAIN_DIR . 'logs/error.log';
+ini_set('error_log', $error_log);
+
+// Make shure sessions folders exist
 if (!is_dir(MAIN_DIR . 'sessions')){
 	@mkdir(MAIN_DIR . 'sessions');
-	if (!is_dir(MAIN_DIR . 'sessions')){
-		throw new Exception('Cannot create sessions folder');
-	}
 }
-
-//SETS SESSION SAVE PATH
-ini_set ( 'session.save_path', MAIN_DIR . 'sessions' );
-
-// TURN OFF ERROR DISPLAY
-ini_set('display_errors', 'off');
+if (is_dir(MAIN_DIR . 'sessions') && is_writable(MAIN_DIR . 'sessions') ){
+	//SETS SESSION SAVE PATH
+	ini_set ( 'session.save_path', MAIN_DIR . 'sessions' );
+}
 
 //SETS SESSION MAX LIFE TIME
 ini_set('session.gc_maxlifetime', (3600*8)); #8h
+
+
+// SETS ERROR REPORTING
+( ini_get('error_reporting') <> 6135 )	?	ini_set('error_reporting', 6135) : '';
+// TURN OFF ERROR DISPLAY
+ini_set('display_errors', 'off');
+
 
 //START SESSION
 session_start();
 
 /*
- * Session CONSTANTS
+ * Set APP from REQUEST['app']
+ * APP dir must exist!
  */
-($_REQUEST['app'] && is_dir('./projects/' . $_REQUEST['app'])) ? $_SESSION['app'] = $_REQUEST['app'] : '';
-
-define ( 'PREFIX_DELIMITER', '__');
-
-if($_SESSION['app']) {
-	define ( 'PROJ_DIR', MAIN_DIR . "projects/{$_SESSION['app']}/");
+if ( isset($_REQUEST['app']) && is_dir('./projects/' . $_REQUEST['app'] ) ) {
+	define ( 'APP', $_REQUEST['app']);
+} elseif (isset($_SESSION['app'])) {
 	define ( 'APP', $_SESSION['app']);
-	define ( 'PREFIX', APP . PREFIX_DELIMITER);
 }
-
-/*
- * PROJect CONSTANTS
+/**
+ * Define PROJ_DIR and PREFIX: APP dependent
  */
-if ( defined('PROJ_DIR')) {
-	define ('PROJ_TMP_DIR',		PROJ_DIR . 'tmp/' . $_SESSION['user']['id'] . '/');
-	$error_log 				= 	PROJ_DIR . 'error.log';
+if(defined('APP') && isset($_SESSION['user']['id']) ) {
 
+	$_SESSION['app'] = APP;
+	define ( 'PREFIX', APP . PREFIX_DELIMITER);
+
+	define ( 'PROJ_DIR', 		MAIN_DIR . 'projects/' . APP . '/');
+	define ( 'PROJ_TMP_DIR',	MAIN_DIR . 'projects/' . APP . '/tmp/' . $_SESSION['user']['id'] . '/');
+	$error_log 				= 	MAIN_DIR . 'projects/' . APP . '/error.log';
+	ini_set('error_log', $error_log);
 
 	/*
 	 * Create directories that MUST exist
@@ -74,14 +79,12 @@ if ( defined('PROJ_DIR')) {
 
 	foreach($must_exist_dirs as $dir) {
 		if (!is_dir($dir)) {
-			mkdir($dir, 0777, true);
+			@mkdir($dir, 0777, true);
 		}
 	}
 }
 
-// LOG PHP ERRORS TO APPLICATION ERROR.LOG FILE
 define ('ERROR_LOG', $error_log);
-ini_set('error_log', ERROR_LOG);
 
 
 /**
