bf368964ceb901b37e86237ec196eaf0e3813199
Julian Bogdani <jbogdani@gmail.com>
2020-08-05 10:01:40 +0200

    Inline docs enhanced

--- index.php
+++ index.php
@@ -4,6 +4,18 @@
  * @copyright		BraDypUS, Julian Bogdani <jbogdani@gmail.com>
  * @license			See file LICENSE distributed with this code
  * @since			Apr 8, 2012
+ * 
+ * List of system query parameters:
+ * 	GET: mini:		if 1 minifies all js scripts
+ * 	GET: logout:	if 1/true/set forces user logout
+ * 	GET: debug:		if 1 starts debug_mode (sets SESSION: debug_mode)
+ * 					if 0 stops debug_mode (unsets SESSION: debug_mode)
+ * 
+ * Controller related
+ * REQUEST: obj		Object name to run
+ * REQUEST: method	Method name to run
+ * REQUEST: param	Params to pass to object::method
+ * 
  */
 
 ob_start();
