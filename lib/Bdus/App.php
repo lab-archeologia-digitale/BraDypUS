5ceda3adbda22b3858487035898c4b14d00b5c02
Julian Bogdani <jbogdani@gmail.com>
2020-08-25 00:02:27 +0200

    Dropped support for controllers not extending Controller

--- lib/Bdus/App.php
+++ lib/Bdus/App.php
@@ -39,25 +39,6 @@ class App
 		// Set method
         $method = $this->get['method'] ?: 'showAll';
 
-        // PARAMS SHOULD DIE!!!
-        if (isset($this->request['param']) && is_string($this->request['param'])) {
-			// get or post single (string) param
-			$param = [ $this->request ];
-
-		} else if (!isset($this->request['param']) && empty($this->post)) {
-			// no post and no no post/get param
-			$param = [];
-
-		} else if (!isset($this->request['param']) && !empty($this->post)) {
-			// no post/get param, but post data
-			$param = [ $this->post ];
-
-		} else if (isset($this->request['param']) && !empty($this->post)) {
-			$param = array( array_merge($this->post, $this->request['param']));
-		} else {
-			$param = $this->request['param'];
-		}
-		
 		try {
 
 			if (!method_exists($obj, $method)){
@@ -71,10 +52,7 @@ class App
 				$_aa->setLog($this->log);
                 $_aa->$method();
             } else {
-				// TODO: should die
-				$this->log->error("Called deprecated method", ["obj" => $obj, "method" => $method, "param" => $param]);
-                call_user_func_array( [$obj, $method], $param);
-                // throw new Exception("Called object {$obj} *must* extend Controller. No direct access is available");
+                throw new \Exception("Called object {$obj} *must* extend Controller. No direct access is available");
             }
 		} catch(\Throwable $e) {
 			$this->log->info($e);
