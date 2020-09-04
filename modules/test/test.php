fa887ed94ed1f022708d892f9b45577c0bf98da8
Julian Bogdani <jbogdani@gmail.com>
2020-09-04 23:07:33 +0200

    First version of UAC

--- modules/test/test.php
+++ modules/test/test.php
@@ -6,13 +6,12 @@
  * @since			Apr 15, 2012
  */
 
+
 class test_ctrl extends Controller
 {
 
 	public function test()
 	{	
-		
-			
 		// $firephp = new Monolog\Handler\FirePHPHandler();
 		// $this->log->pushHandler($firephp);
 		// $this->log->error('Error PHP', ["hello" => "world"]);
@@ -35,6 +34,156 @@ class test_ctrl extends Controller
 
 		echo '<hr />end of test_ctrl::test()';
 	}
+
+    private function testUAC()
+    {
+        $uac = new \UAC\UAC( $this->cfg->get('main.status'), utils::is_online(), $this->db );
+        $actions = $uac->available_actions;
+
+        // // Pure GLOBAL
+        // echo $this->runTest($uac, "global:SUPERADM", [
+        //     'global' => \UAC\UAC::SUPERADM
+        // ]);
+        
+        // echo $this->runTest($uac, "global:ADM", [
+        //     'global' => \UAC\UAC::ADM
+        // ]);
+
+        // echo  $this->runTest($uac, "global:UPDATE", [
+        //     'global' => \UAC\UAC::UPDATE
+        // ]);
+
+        // echo $this->runTest($uac, "global:CREATE", [
+        //     'global' => \UAC\UAC::CREATE
+        // ]);
+        
+        // echo $this->runTest($uac, "global:CREATE", [
+        //     'global' => \UAC\UAC::CREATE
+        // ]);
+
+        // echo $this->runTest($uac, "global:READ", [
+        //     'global' => \UAC\UAC::READ
+        // ]);
+        
+        // echo $this->runTest($uac, "global:ENTER", [
+        //     'global' => \UAC\UAC::ENTER
+        // ]);
+
+        // // GLOBAL rewriting table
+        // echo $this->runTest($uac, "global:SUPERADM , sitarc__siti:READ", [
+        //     'global' => \UAC\UAC::SUPERADM,
+        //     'sitarc__siti' => \UAC\UAC::READ,
+        // ]);
+
+        // echo $this->runTest($uac, "global:ADM , sitarc__siti:READ", [
+        //     'global' => \UAC\UAC::ADM,
+        //     'sitarc__siti' => \UAC\UAC::READ,
+        // ]);
+
+        // echo $this->runTest($uac, "global:UPDATE , sitarc__siti:READ", [
+        //     'global' => \UAC\UAC::UPDATE,
+        //     'sitarc__siti' => \UAC\UAC::READ,
+        // ]);
+
+        // // GLOBAL rewriting table
+        // echo $this->runTest($uac, "global:READ , sitarc__siti:UPDATE", [
+        //     'global' => \UAC\UAC::READ,
+        //     'sitarc__siti' => \UAC\UAC::UPDATE,
+        // ]);
+
+        // echo $this->runTest($uac, "global:READ , sitarc__siti:UPDATE", [
+        //     'global' => \UAC\UAC::READ,
+        //     'sitarc__siti' => \UAC\UAC::UPDATE,
+        // ]);
+
+        // GLOBAL, table, on table
+        // echo $this->runTest( $uac,  "global:ENTER, sitarc__siti:CREATE", 
+        //     [
+        //         'global' => \UAC\UAC::ENTER,
+        //         'sitarc__siti' => \UAC\UAC::CREATE,
+        //     ],
+        //     'sitarc__siti'
+        // );
+        // echo $this->runTest( $uac,  "global:ENTER, sitarc__siti:READ", 
+        //     [
+        //         'global' => \UAC\UAC::ENTER,
+        //         'sitarc__siti' => \UAC\UAC::READ,
+        //     ],
+        //     'sitarc__siti'
+        // );
+        // echo $this->runTest( $uac,  "global:ENTER, sitarc__siti:UPDATE", 
+        //     [
+        //         'global' => \UAC\UAC::ENTER,
+        //         'sitarc__siti' => \UAC\UAC::UPDATE,
+        //     ],
+        //     'sitarc__siti'
+        // );
+        // echo $this->runTest( $uac,  "global:ENTER, sitarc__siti:DELETE", 
+        //     [
+        //         'global' => \UAC\UAC::ENTER,
+        //         'sitarc__siti' => \UAC\UAC::DELETE,
+        //     ],
+        //     'sitarc__siti'
+        // );
+
+
+        echo $this->runTest( $uac,  "global:ENTER, sitarc__siti:READ (id < 2)", 
+            [
+                'global' => \UAC\UAC::ENTER,
+                'sitarc__siti' => [\UAC\UAC::READ, 'id < 2'],
+            ],
+            'sitarc__siti', 1
+        );
+        echo $this->runTest( $uac,  "global:ENTER, sitarc__siti:UPDATE (id < 2)", 
+            [
+                'global' => \UAC\UAC::ENTER,
+                'sitarc__siti' => [\UAC\UAC::UPDATE, 'id < 2'],
+            ],
+            'sitarc__siti', 1
+        );
+        echo $this->runTest( $uac,  "global:ENTER, sitarc__siti:DELETE (id < 2)", 
+            [
+                'global' => \UAC\UAC::ENTER,
+                'sitarc__siti' => [\UAC\UAC::DELETE, 'id < 2'],
+            ],
+            'sitarc__siti', 1
+        );
+
+
+       
+
+        
+
+        return;
+
+    }
+    
+    private function runTest( \UAC\UAC $uac, 
+        string $label, 
+        array $user, 
+        string $onTable = null, 
+        int $onRecId = null, 
+        bool $user_owns_record = false
+    )
+    {
+        
+        $actions = $uac->available_actions;
+
+        $html = "Testing user " . $label . " on " . ( $onTable ? $onTable : 'system') .
+            ($onRecId ? " on record $onRecId" : '') .
+            ($user_owns_record ? " where users OWNS record" : '') .
+            "<ul>";
+        foreach ($actions as $action) {
+            $ret = $uac->user( $user )->can( $action, $onTable, $onRecId, $user_owns_record );
+            $html .= '<li class="text-' . ( $ret ? 'success' : 'danger') . '">' .
+                "Can <strong>$action</strong>: " . 
+                ( $ret ? '&#10004; ' : '&#10006;') .
+                '</li>';
+        }
+        $html .= "</ul>";
+
+        return $html;
+    }
 
 	private function testShortSQL()
 	{
