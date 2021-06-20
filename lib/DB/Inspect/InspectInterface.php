4a024bf6aa668c96be554dda8a519c487e58b4fb
Julian Bogdani <jbogdani@gmail.com>
2021-06-20 21:47:32 +0200

    Inline documentation for \Inspect

--- lib/DB/Inspect/InspectInterface.php
+++ lib/DB/Inspect/InspectInterface.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @copyright 2007-2021 Julian Bogdani
  * @license AGPL-3.0; see LICENSE
@@ -11,19 +12,24 @@ namespace DB\Inspect;
  */
 interface InspectInterface
 {
-    /**
-     * Checks if table exists
-     */
-    public function tableExists(string $tb): bool;
+  /**
+   * Checks if table exists
+   */
+  public function tableExists(string $tb): bool;
 
-    /**
-     * Gets list of table columns 
-     *
-     * @param string $tb    Table name
-     * @return array array of arrays with column data
-     *                  example: [ [ 'fld' => "fld_name", 'type' => 'column_type' ], [ ...etc... ] ]
-     */
-    public function tableColumns(string $tb): array;
+  /**
+   * Gets list of table columns 
+   *
+   * @param string $tb    Table name
+   * @return array array of arrays with column data
+   *                  example: [ [ 'fld' => "fld_name", 'type' => 'column_type' ], [ ...etc... ] ]
+   */
+  public function tableColumns(string $tb): array;
 
-    public function getAllTables() : array;
+  /**
+   * Returns full list of tables contained in database
+   *
+   * @return array
+   */
+  public function getAllTables(): array;
 }
--- lib/DB/Inspect/Postgres.php
+++ lib/DB/Inspect/Postgres.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @copyright 2007-2021 Julian Bogdani
  * @license AGPL-3.0; see LICENSE
@@ -7,55 +8,56 @@
 namespace DB\Inspect;
 
 use DB\DBInterface;
+
 /**
  * Interface to interact with database structure
  */
 class Postgres implements InspectInterface
-{   
-    private $db;
+{
+  private $db;
 
-    public function __construct(DBInterface $db)
-    {
-        $this->db = $db;
+  public function __construct(DBInterface $db)
+  {
+    $this->db = $db;
+  }
+
+  public function tableExists(string $tb): bool
+  {
+    $res = $this->db->query(
+      "SELECT COUNT(*) as tot FROM information_schema.tables WHERE table_name = ?",
+      [$tb]
+    );
+
+    return $res && !empty($res) && $res[0]['tot'] > 0;
+  }
+
+  public function tableColumns(string $tb): array
+  {
+    $ret = [];
+
+    $res = $this->db->query("SELECT * FROM information_schema.columns WHERE table_name = ?", [$tb]);
+    if (!$res || empty($res)) {
+      throw new \Exception("Error on getting column list for table {$tb}");
     }
 
-    public function tableExists(string $tb): bool
-    {
-        $res = $this->db->query(
-            "SELECT COUNT(*) as tot FROM information_schema.tables WHERE table_name = ?",
-            [ $tb ]
-        );
-        
-        return $res && !empty($res) && $res[0]['tot'] > 0;
+    foreach ($res as $row) {
+      $ret[] = [
+        'fld' => $row['column_name'],
+        'type' => $row['data_type']
+      ];
     }
 
-    public function tableColumns(string $tb): array
-    {
-        $ret = [];
+    return $ret;
+  }
 
-        $res = $this->db->query("SELECT * FROM information_schema.columns WHERE table_name = ?", [ $tb ]);
-        if (!$res || empty($res)){
-            throw new \Exception("Error on getting column list for table {$tb}");
-        }
+  public function getAllTables(): array
+  {
+    $res = $this->db->query("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND  schemaname != 'information_schema'");
 
-        foreach ($res as $row) {
-            $ret[] = [
-                'fld' => $row['column_name'],
-                'type' => $row['data_type']
-            ];
-        }
-
-        return $ret;
+    $ret = [];
+    foreach ($res as $row) {
+      array_push($ret, $row['tablename']);
     }
-
-    public function getAllTables() : array
-    {
-        $res = $this->db->query("SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND  schemaname != 'information_schema'");
-        
-        $ret = [];
-        foreach ($res as $row) {
-            array_push($ret, $row['tablename']);
-        }
-        return $ret;
-    }
+    return $ret;
+  }
 }
